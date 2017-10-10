73bbe47d24e697f7cd3a8ae67d30954ede43fd72
Humphrey <humphrey.chen@mkcorp.com>
2017-10-10 10:39:59 +0800

    release setup

---
+++ AuctionConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuctionConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AuctionSocketClientConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuctionClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8300;
+            var host = "10.80.65.44";
+            var pre = "10.80.65.44:8300:";
+            var ip = IPAddress.Parse(host);
+            var ipe = new IPEndPoint(ip, port);
+
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(ipe);
+
+            //receive message
+            new Task(()=> 
+            {
+                while(true)
+                {
+                    string recStr = "";
+                    byte[] recBytes = new byte[4096];
+                    int bytes = clientSocket.Receive(recBytes, recBytes.Length, 0);
+                    recStr += Encoding.UTF8.GetString(recBytes, 0, bytes);
+                    Console.WriteLine(ipe + " " + recStr);
+                    Thread.Sleep(100);
+                }
+            }).Start();
+
+            Console.Read();
+            clientSocket.Close();
+        }
+    }
+}
